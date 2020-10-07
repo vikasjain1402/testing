2fa24a718855ef4a858fe9021c9365ae0fe53047
John Doe <johndoe@example.com>
2020-10-07 13:48:44 +0530

    initialize folder

---
+++ keys.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os
+os.system('xmodmap -e "keycode 108 =less"')
+os.system('xmodmap -e "keycode 135 =comma"')
+
+
+password="1@Million"
+
+command="sudo -S apt update"
+print("Executing Update...")
+os.system(f"echo {password} |{command}")
+print("Done\n\n")
+
+command="sudo -S apt upgrade -y"
+print("Executing Upgrade....")
+os.system(f"echo {password} |{command}")
+print("Done")
+
+
